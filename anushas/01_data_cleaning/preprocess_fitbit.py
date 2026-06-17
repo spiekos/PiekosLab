853a0ded56f3dd77106685e2281c5182df45192d
Anusha Sundar <anushas@brb-00972.apn.wlan.private.upenn.edu>
2026-06-17 14:59:20 -0400

    extract data table and drop appropriate patients

--- anushas/01_data_cleaning/preprocess_fitbit.py
+++ anushas/01_data_cleaning/preprocess_fitbit.py
@@ -1,9 +1,10 @@
+import io
 import pandas as pd
 
 # loads both fitbit data sheets and returns both sheets
 def load_sheets():
-    sheet1 = pd.read_csv("01_data_cleaning/preprocess_fitbit_data/DP3_playset.csv", index_col = 0)
-    sheet2 = pd.read_csv("01_data_cleaning/preprocess_fitbit_data/DP3-FitbitFullReport_DATA_LABELS_2025-02-18_1356.csv")
+    sheet1 = pd.read_csv("00_raw_data/DP3_playset.csv", index_col = 0)
+    sheet2 = pd.read_csv("00_raw_data/DP3-FitbitFullReport_DATA_LABELS_2025-02-18_1356.csv")
     return sheet1, sheet2
 
 # rename columns of both sheets so that column names are consistent across sheets
@@ -54,8 +55,8 @@ def sort_columns(sheet):
 
 # reads from the fitbit analysis log file
 # extracts the table containing data on max number of consecutive days missing per feature per patient, and returns this table
-def read_fitbit_analysis():
-    fitbit_analysis_path = "02_exploratory_analysis/analyze_fitbit_data/log.txt"
+def extract_days_missing_table():
+    fitbit_analysis_path = "02_exploratory_analysis/outputs/fitbit_data_analysis.txt"
     target_title = "Maximum consecutive number of days missing per feature per patient:"
 
     table_lines = []
@@ -78,29 +79,44 @@ def read_fitbit_analysis():
             if inside_target_table:
                 table_lines.append(line)
 
-        
+    # convert into a dataframe
+    table_data = "".join(table_lines)
 
+    # read the csv, treating any sequence of spaces as a separator
+    df_target = pd.read_csv(io.StringIO(table_data), sep = r'\s+')
+
+    df_target.columns = df_target.columns.str.strip()
+
+    return df_target
 
 # drop all patients with no recording of 2+ days in a row for any feature
-# sheet: the entire fitbit data sheet, which contains patient information and all collected metrics
-# table: the dataframe containing data on max number of consecutive days missing per feature per patient
+# sheet: the fitbit data sheet, which contains patient information and all collected metrics
+# table: the dataframe containing a table of max number of consecutive days missing per feature per patient
 def drop_patients(sheet, table):
+    metric_cols = [col for col in table.columns if col != "id"]
 
+    # find rows for which any metric value is >= 2
+    drop_mask = (table[metric_cols] >= 2).any(axis = 1)
 
+    # find the unique IDs of the patients who failed this test
+    patients_to_drop = table.loc[drop_mask, "id"].unique()
 
+    # drop the appropriate patients
+    sheet_clean = sheet[~sheet["Record ID"].isin(patients_to_drop)]
 
+    return sheet_clean, patients_to_drop
 
 def main():
     sheet1, sheet2 = load_sheets()
     merged = merge_sheets(sheet1, sheet2)
     merged = sort_columns(merged)
 
+    days_missing_table = extract_days_missing_table()
+
+    merged_clean, patients_to_drop = drop_patients(merged, days_missing_table)
+
     # write sheet to an output file
-    merged.to_csv("01_data_cleaning/preprocess_fitbit_data/output.csv", index = False)
-
-
-
-
+    merged_clean.to_csv("01_data_cleaning/processed_data/processed_fitbit_data.csv", index = False)
 
 if __name__ == "__main__":
     main()
