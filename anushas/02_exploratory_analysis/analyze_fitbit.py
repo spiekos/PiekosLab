6a31abda3f1f629532d05e172aa6221f6a83b82c
Anusha Sundar <anushas@MacBook-Air-133.local>
2026-07-01 19:26:33 -0400

    prepared data for correlation test

--- anushas/02_exploratory_analysis/analyze_fitbit.py
+++ anushas/02_exploratory_analysis/analyze_fitbit.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-# load and return the dataset
-def load_sheet():
-    sheet = pd.read_csv("01_data_cleaning/processed_data/processed_fitbit_data.csv")
-    return sheet
+# load and return the fitbit dataset and the placental dataset
+def load_sheets():
+    sheet1 = pd.read_csv("01_data_cleaning/processed_data/processed_fitbit_data.csv")
+    sheet2 = pd.read_csv("01_data_cleaning/processed_data/processed_placental_data.csv")
+    sheet3 = pd.read_csv("00_raw_data/dp3 master table v2.xlsx - variables of interest.csv")
+    return sheet1, sheet2, sheet3
 
 # filters the sheet to only include "Fitbit Data" events and only include events during pregnancy
 def filter_sheet(sheet):
@@ -118,9 +120,7 @@ def calc_summary_stats(sheet, feature_cols):
 # returns a table containing the number of patients with Fitbit data for at least one metric during each timeframe
 # the timeframes are: 1st trimester, early 2nd trimester, late 2nd/early 3rd trimester, late 3rd trimester
 # @param sheets: a list containing four datasets, each containing the data for one of the above timeframes
-def get_patients_per_timeframe(sheets, feature_cols):
-    timeframe_names = ["First Trimester", "Early Second Trimester", "Late Second and Early Third Trimester", "Late Third Trimester"]
-
+def get_patients_per_timeframe(sheets, feature_cols, timeframe_names):
     summary_data = []
 
     for df, timeframe in zip(sheets, timeframe_names):
@@ -184,6 +184,69 @@ def get_metric_representation_matrices(sheet, feature_cols):
 
     return final_table, pt_summary, metric_summary
 
+# collapses multi-row trimester data into single-row patient averages for each metric
+# merges this with delivery and placental data
+# filters to only include patients who have placental reports
+# returns a table containing fitbit data (averaged by metric by trimester) and delivery + placental metrics by patient
+# table contains all timeframes; column names reflect which timeframe the data was collected in
+def prepare_correlation_data(sheet_bucketed, feature_cols, timeframe_names, clinical_raw, placental_raw):
+    trimester_dfs = []
+
+    # collapse each trimester bucket into metric averages by patient
+    for df, label in zip(sheet_bucketed, timeframe_names):
+        if df.empty:
+            continue
+
+        # take the mean of each metric by patient in this timeframe
+        # rename the columns
+        pt_trimester_avg = df.groupby("Record ID")[feature_cols].mean()
+        pt_trimester_avg.columns = [f"{label}_{col}" for col in pt_trimester_avg.columns]
+        trimester_dfs.append(pt_trimester_avg)
+
+    if not trimester_dfs:
+        print("Warning: No trimester data found to collapse.")
+        return pd.DataFrame()
+    
+    # join all trimesters side-by-side
+    fitbit_pivoted = pd.concat(trimester_dfs, axis = 1).reset_index()
+
+    # isolate target clinical variables
+    clinical_targets = [
+        "maternal age", "height (cm)", "weight (kg)", "delivery bmi", "prepregnancy weight self or record", "prepregnancy BMI self or record", 
+        "gravida", "parity", "diabetes", "chtn", "Route of delivery 1-vag, 2-cs", "gest age del", "birthweight", "apgar 1", "apgar 5", "nicu days"
+    ]
+    # keep only the columns that exist in the spreadsheet
+    existing_clinical = [col for col in clinical_targets if col in clinical_raw.columns]
+    # aggregate all clinical data for each patient into a dataframe
+    clinical_clean = clinical_raw.rename(columns = {"ID": "Record ID"})
+    clinical_clean = clinical_clean.groupby("Record ID")[existing_clinical].first().reset_index()
+
+    # isolate target placental variables
+    placental_targets = [
+        "placental infarction", "distal villous hypoplasia focal/diffuse", "accelerated villous maturation", "increased syncytial knots", 
+        "decidual arteriopathy membrane role/basal plate/both", "segmental avascular villi small/intermediate/large", "delayed villous maturation", 
+        "maternal inflammatory response stage/grade", "villitis of unknown etiology, high/low grade, focal/diffuse", "increased perivillous fibrin deposition", 
+        "chorangiosis"
+    ]
+    # keep only the columns that exist in the spreadsheet
+    existing_placental = [col for col in placental_targets if col in placental_raw.columns]
+    # aggregate all placental data for each patient into a dataframe
+    placental_clean = placental_raw.rename(columns = {"id": "Record ID"})
+    placental_clean = placental_clean.groupby("Record ID")[existing_placental].first().reset_index()
+
+    # merge fitbit, clinical, and placental data
+    master_corr_df = pd.merge(fitbit_pivoted, clinical_clean, on = "Record ID", how = "inner")
+    master_corr_df = pd.merge(master_corr_df, placental_clean, on = "Record ID", how = "inner")
+
+    # determine which placental columns survived the merge process
+    final_placental_cols = [col for col in existing_placental if col in master_corr_df.columns]
+    
+    if final_placental_cols:
+        # drop rows for which all placental metrics are missing
+        master_corr_df = master_corr_df.dropna(subset = final_placental_cols, how = "all")
+    
+    return master_corr_df
+
 # print all calculated data into a log file
 def print_log(total_patients, total_missing, per_patient, max_con_missing, unique_dates, summary_stats, 
               patients_per_timeframe, metric_matrix, pt_summary, metric_summary, num_metrics):
@@ -231,9 +294,9 @@ def print_log(total_patients, total_missing, per_patient, max_con_missing, uniqu
         f.write("\n\n")
 
 def main():
-    sheet = load_sheet()
+    fitbit_sheet, placental_sheet, clinical_sheet = load_sheets()
 
-    sheet_filtered = filter_sheet(sheet)
+    sheet_filtered = filter_sheet(fitbit_sheet)
 
     feature_cols = [col for col in sheet_filtered.columns if col.startswith(("Activities", "Sleep", "Heart Rate"))]
 
@@ -243,6 +306,8 @@ def main():
     sheet_filtered["Gestational age by reported LMP"] = pd.to_numeric(sheet_filtered["Gestational age by reported LMP"], errors = "coerce")
     sheet_filtered["gest age del"] = pd.to_numeric(sheet_filtered["gest age del"], errors = "coerce")
 
+    timeframe_names = ["First Trimester", "Early Second Trimester", "Late Second and Early Third Trimester", "Late Third Trimester"]
+
     sheet_bucketed = bucket_data(sheet_filtered)
 
     total_patients = get_total_patients(sheet_filtered)
@@ -251,11 +316,16 @@ def main():
     max_con_missing = get_max_consecutive_missing(sheet_filtered, feature_cols)
     unique_dates = count_unique_dates(sheet_filtered)
     summary_stats = calc_summary_stats(sheet_filtered, feature_cols)
-    patients_per_timeframe = get_patients_per_timeframe(sheet_bucketed, feature_cols)
+    patients_per_timeframe = get_patients_per_timeframe(sheet_bucketed, feature_cols, timeframe_names)
     metric_matrix, pt_summary, metric_summary = get_metric_representation_matrices(sheet_filtered, feature_cols)
 
     print_log(total_patients, total_missing, per_patient, max_con_missing, unique_dates, summary_stats, 
               patients_per_timeframe, metric_matrix, pt_summary, metric_summary, len(feature_cols))
+
+    correlation_ready_df = prepare_correlation_data(sheet_bucketed, feature_cols, timeframe_names, clinical_sheet, placental_sheet)
+    if not correlation_ready_df.empty:
+        output_csv_path = "02_exploratory_analysis/outputs/master_fitbit_clinical_correlation_data.csv"
+        correlation_ready_df.to_csv(output_csv_path, index = False)
 
 if __name__ == "__main__":
     main()
